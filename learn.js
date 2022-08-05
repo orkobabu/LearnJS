a18385388b07fa90560566cf9bcde82436ff9bcd
orkobabu <mofizul.orko@gmail.com>
2022-08-05 18:33:09 +0600

    variabl

---
+++ learn.js
@@ -0,0 +1,77 @@
+console.log('hello');
+console.log('bangladesh');
+
+var bananaPrice = 20;
+console.log(bananaPrice);
+var mobilePrice = 50055;
+console.log(mobilePrice);
+
+var seenAfter = 45;
+console.log(typeof seenAfter);
+
+var name = "Shohidul Islam";
+console.log(name);
+
+var name = "Shohidul Islam";
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+var offDay = "no";
+var busyDay = "yes";
+console.log(isHot);
+console.log(isRich);
+console.log(offDay);
+console.log(busyDay);
+console.log(typeof isHot);
+console.log(typeof busyDay);
+
+var wordCase = "We use different technologies on our website. Some of them are essential and thus necessary."
+console.log(wordCase.toLowerCase());
+console.log(wordCase.toUpperCase());
+console.log(wordCase.indexOf("of"));
+console.log(wordCase.split("our"));
+
+var number1 = 30;
+var number2 = 13.6;
+console.log(number1 + number2);
+
+var number1 = 30;
+var number2 = '13.6';
+number2 = parseFloat(number2);
+console.log(number1 + number2);
+
+var number1 = 30;
+var number2 = '13.6';
+number2 = parseInt(number2);
+console.log(number1 + number2);
+
+var number1 = 30;
+var number2 = '13.6';
+console.log(number1 + (+number2));
+
+var number1 = 30;
+var number2 = 13.6;
+number1 = '' + number1;
+console.log(number1);
+
+var number1 = 30;
+var number2 = 13.6;
+number1 = '' + number1;
+console.log(typeof number1);
+
+var number1 = 0.6;
+var number2 = 2.2;
+var total = number1 + number2;
+console.log(total);
+
+var number1 = 0.6;
+var number2 = 2.2;
+var total = number1 + number2;
+total = total.toFixed(1);
+console.log(total);
+
+var price1 = 'Mofizul';
+var name = 'Islam';
+var result = price1 + ' ' + name;
+console.log(result);
